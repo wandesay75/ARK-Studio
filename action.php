9809c53e000b95a2bbd4cf2bb32fe150092daede
Alvin Austin <68804883+wandesay75@users.noreply.github.com>
2022-06-16 20:09:58 +0700

    first commit

---
+++ action.php
@@ -0,0 +1,5 @@
+<?php
+if (isset($_POST['kirim'])) {
+    echo "<script>alert('Data anda telah berhasil di kirim, silahkan tunggu balasan di email anda');window.location='contact.html'</script>";
+}
+?>
